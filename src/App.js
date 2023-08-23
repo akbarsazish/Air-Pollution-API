60b08466d0d79414b4ea217e8a350319e6bc1f27
akbarsazish <sazishaliakbar@gmail.com>
2023-08-24 00:49:02 +0330

    changes to routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
 import './assets/style.css';
 import Home from './components/pages/Home';
 import Details from './components/pages/Details';
@@ -7,12 +6,10 @@ import Details from './components/pages/Details';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/:country" element={<Details />} />
         </Routes>
-      </BrowserRouter>
     </div>
   );
 }
