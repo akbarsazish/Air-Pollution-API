d625792ca7c5c8031e9c03f2e6d36d2b924e338e
akbarsazish <sazishaliakbar@gmail.com>
2023-08-24 00:48:13 +0330

    add brouser router "

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
