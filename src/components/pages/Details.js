ead19e45fe394b47ff3e7c4c243ed1b9458e3798
akbarsazish <sazishaliakbar@gmail.com>
2023-08-24 00:40:49 +0330

    add Home page and details page and configure routes

---
+++ src/components/pages/Details.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Details() {
+    return (
+      <div>
+        <h1> Hello details </h1>
+      </div>
+    );
+}
+
+export default Details;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Home() {
+    return (
+      <>
+        <h1> this is home page </h1>
+      </>
+    );
+  }
+
+export default Home;
